dcb023cbd6d57be3a77c08236c00a07225556f74
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-07-31 15:02:49 -0700

    using loops to read in lines of file

--- while_read.sh
+++ while_read.sh
@@ -2,8 +2,13 @@
 
 # Read lines from file
 
+# use read to input the fields from the redirect file
+# read will exit after each line is read
 while read name age; do
     printf "Name: %s\tAge: %s\n" \
     $name \
     $age
+# add redirection operator after done statement
+# once a non-zero exit status is reached the loop will terminate
 done < testing.txt
+
