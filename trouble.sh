ab594bf2e53014d745125f9b15cfd76dac0bfba3
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-08-02 07:05:54 -0700

    helpful tools for troubleshooting / debugging

--- trouble.sh
+++ trouble.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# Script to demo common errors
+# adding -x after #!/bin/bash will enable tracing for the entire script
+# tracing allows us to see the commands performed with expansion applied
 
-number=
+# the default leading + sign is contained in the PS4 (prompt string 4) shell variable
+# we can adjust it to include the current line number in the script where the trace is performed
+export PS4='$LINENO '
+
+
+number=1
+
+# can also turn on tracing for specific portions of the script
+set -x 
 
 # if accepts a list of commands and evalutes the exit code of the last command in the list
 # need to follow [] by a semicolon otherwise the shell will read then and the next echo line as part of the if command
@@ -15,3 +24,6 @@ if [ "$number" = 1 ]; then
 else
     echo "Number is not equal to 1"
 fi
+
+# turns off tracing
+set +x 
