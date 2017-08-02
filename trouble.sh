449a9f517f2596be890ae6d4b6c3d032b3ca0097
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-08-01 17:22:27 -0700

    looking at common errors

--- trouble.sh
+++ trouble.sh
@@ -2,9 +2,15 @@
 
 # Script to demo common errors
 
-number=1
+number=
 
-if [ $number = 1 ]; then
+# if accepts a list of commands and evalutes the exit code of the last command in the list
+# need to follow [] by a semicolon otherwise the shell will read then and the next echo line as part of the if command
+
+# also should surround $number with quotes because if
+# the number variable is empty it will still show up as an empty string
+# and the shell can evaluate the binary operator =
+if [ "$number" = 1 ]; then
     echo "Number is equal to 1"
 else
     echo "Number is not equal to 1"
