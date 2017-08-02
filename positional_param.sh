22932cbec2e1b4baf26e76ee7d794be5d6bf8b64
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-08-02 09:53:27 -0700

    using $* and $@ to unpack args

--- positional_param.sh
+++ positional_param.sh
@@ -19,3 +19,20 @@ Number of arguments: $#
 \$9 = $9
 \$10 = ${10}
 "
+
+print_params () {
+    echo "\$1 = $1"
+    echo "\$2 = $2"
+    echo "\$3 = $3"
+    echo "\$4 = $4"
+}
+
+pass_params () {
+    # -e allows echo to interpret backslash escapes
+    echo -e "\n" '$* : ';   print_params $*
+    echo -e "\n" '"$*" : ';  print_params "$*"
+    echo -e "\n" '$@ : ';   print_params $@
+    echo -e "\n" '"$@" :';  print_params "$@"
+}
+
+pass_params "word" "words with spaces"
