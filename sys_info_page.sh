7e7183d6a445999b312b0911ea1451d77b4970a1
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-08-03 16:22:11 -0700

    sticking to echo since cat _EOF_ not cooperating

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Program to output a system info page
 
@@ -7,39 +7,29 @@ CURRENT_TIME=$(date +"%x %r %Z")
 TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
 report_uptime () {
-cat << _EOF_
-<h2>System Uptime</h2>
-<pre>$(uptime)</pre>
-_EOF_
-return
+    "<h2>System Uptime</h2>
+    <pre>$(uptime)</pre>"
+    return
 }
 
 report_disk_space () {
-cat << _EOF_
-<h2>Disk Space Utilization</h2>
-<pre>$(df -h)</pre>
-_EOF_
-return
+    echo "<h2>Disk Space Utilization</h2>
+    <pre>$(df -h)</pre>"
+    return
 }
 
 report_home_space () {
-if [[$(id -u) -eq 0 ]]; then
-cat << _EOF_
-<h2>Home Space Utilization (All Users)</h2>
-<pre>$(du -sh /Users/*)</pre>
-_EOF_
-else
-cat << _EOF_
-<h2>Home Space Utilization ($USER)</h2>
-<pre>$(du -sh $HOME)</pre>
-_EOF_
-fi
-return
+    if [[$(id -u) -eq 0 ]]; then
+        echo "<h2>Home Space Utilization (All Users)</h2>
+        <pre>$(du -sh /Users/*)</pre>"
+    else
+        echo "<h2>Home Space Utilization ($USER)</h2>
+        <pre>$(du -sh $HOME)</pre>"
+    fi
+    return
 }
 
-
-cat << _EOF_
-<html>
+echo" <html>
     <head>
         <title>$TITLE</title>
     </head>
@@ -50,5 +40,4 @@ cat << _EOF_
         $(report_disk_space)
         $(report_home_space)
     </body>
-</html>
-_EOF_
+</html>"
